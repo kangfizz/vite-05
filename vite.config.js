9321c6f4ac1fda9fe00c5dfa7deb89c8613e10dd
康富城 <kangfizz@ap-mic.com>
2023-05-25 17:11:04 +0800

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite-05/",
+  base: process.env.NODE_ENV === 'production' ? "/vite-05/" : "/",
   plugins: [vue()],
 })
